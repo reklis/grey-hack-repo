f5dae4be3baf01893f66953ef79e879fae22f71a
sfusco <slf@stevenfusco.com>
2025-12-25 20:16:05 -0500

    searching file on disk

--- app/reflexes/decipher_reflex.rb
+++ app/reflexes/decipher_reflex.rb
@@ -1,13 +1,32 @@
 # frozen_string_literal: true
 
+require "shellwords"
+
 class DecipherReflex < ApplicationReflex
   def deciphe(value)
+    hashes = value.split("\n").map { |line| line.split(":")[-1].strip.downcase }
+    passwords = lookup_passwords(hashes)
+
     result = value.split("\n").map do |line|
       values = line.split(":")
       hash = values[-1].strip.downcase
-      values[-1] = PASSWORDS[hash] || values[-1]
+      values[-1] = passwords[hash] || values[-1]
       values.join(":")
     end.join("\n")
     morph "#result", result || "no result"
+  end
+
+  private
+
+  def lookup_passwords(hashes)
+    return {} if hashes.empty? || !PRECOMPUTED_HASHES_FILE.exist?
+
+    pattern = hashes.map { |h| "^#{Regexp.escape(h)}:" }.join("|")
+    output = `grep -E #{Shellwords.escape(pattern)} #{Shellwords.escape(PRECOMPUTED_HASHES_FILE.to_s)} 2>/dev/null`
+
+    output.lines.each_with_object({}) do |line, result|
+      hash, password = line.chomp.split(":", 2)
+      result[hash] = password if hash && password
+    end
   end
 end
--- config/initializers/passwords_table.rb
+++ config/initializers/passwords_table.rb
@@ -1,16 +1,3 @@
 # frozen_string_literal: true
 
-puts "Loading precomputed password hashes..."
-
-PASSWORDS = {}
-precomputed_file = Rails.root.join("wordlists", "precomputed_hashes.txt")
-
-if precomputed_file.exist?
-  File.foreach(precomputed_file, chomp: true) do |line|
-    hash, password = line.split(":", 2)
-    PASSWORDS[hash] = password if hash && password
-  end
-  puts "Loaded #{PASSWORDS.size} password hashes"
-else
-  puts "Warning: precomputed_hashes.txt not found"
-end
+PRECOMPUTED_HASHES_FILE = Rails.root.join("wordlists", "precomputed_hashes.txt").freeze
