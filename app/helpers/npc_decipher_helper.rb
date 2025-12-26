04072072c731707fcd1245cd6be04fa523544d2c
sfusco <slf@stevenfusco.com>
2025-12-25 20:32:34 -0500

    using wc -l for password count

--- app/helpers/npc_decipher_helper.rb
+++ app/helpers/npc_decipher_helper.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 module NpcDecipherHelper
+  def password_count
+    @password_count ||= begin
+      if PRECOMPUTED_HASHES_FILE.exist?
+        `wc -l < #{PRECOMPUTED_HASHES_FILE}`.to_i
+      else
+        0
+      end
+    end
+  end
 end
